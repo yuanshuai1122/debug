5eb832c28ea10d8ff4741dddb00ea6ffbe315cb6
yuanshuai <yuanshuai@gyyx.cn>
2023-07-08 17:53:20 +0800

    commit

--- transaction/src/main/java/vip/yuanshuai/transaction/TransactionApplication.java
+++ transaction/src/main/java/vip/yuanshuai/transaction/TransactionApplication.java
@@ -2,8 +2,10 @@ package vip.yuanshuai.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class TransactionApplication {
 
     public static void main(String[] args) {
